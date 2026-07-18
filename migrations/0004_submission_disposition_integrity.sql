b5541626fe0f7257e5136b75a3fa7a09926adaed
Yates <pub2free@gmail.com>
2026-07-18 12:26:39 +0800

    fix: make D1 disposition migration remotely compatible

--- migrations/0004_submission_disposition_integrity.sql
+++ migrations/0004_submission_disposition_integrity.sql
@@ -56,30 +56,3 @@ CREATE INDEX idx_submission_dispositions_lead
 	ON submission_disposition_events(lead_id, created_at);
 CREATE INDEX idx_submission_dispositions_archive
 	ON submission_disposition_events(archive_id, created_at);
-
-CREATE TRIGGER submission_disposition_requires_verification
-BEFORE INSERT ON submission_disposition_events
-BEGIN
-	SELECT CASE
-		WHEN NOT EXISTS (
-			SELECT 1 FROM submission_leads
-			WHERE id = NEW.lead_id AND status = '核验中'
-		)
-		THEN RAISE(ABORT, 'final disposition requires a verified submission')
-	END;
-	SELECT CASE
-		WHEN NOT EXISTS (
-			SELECT 1 FROM submission_leads
-			WHERE id = NEW.lead_id
-				AND (
-					(type = '纠错' AND NEW.decision_type IN ('事实修订', '证据身份变更'))
-					OR (type = '权利请求' AND NEW.decision_type = '目录撤回')
-					OR (
-						type = '隐私或安全请求'
-						AND NEW.decision_type IN ('目录撤回', '隐私删除')
-					)
-				)
-		)
-		THEN RAISE(ABORT, 'final disposition is incompatible with submission type')
-	END;
-END;
